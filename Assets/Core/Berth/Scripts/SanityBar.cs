fa3151424a0daa2409a487245a61e61bab25a31b
Verchurro <berrygirault@gmail.com>
2023-04-12 12:36:49 +0200

    com

---
+++ Assets/Core/Berth/Scripts/SanityBar.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SanityBar : MonoBehaviour
+{
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void MaxHealth(int health)
+    {
+       slider.maxValue = health;
+        slider.value = health;
+
+        gradient.Evaluate(1f);
+    }
+
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+}
