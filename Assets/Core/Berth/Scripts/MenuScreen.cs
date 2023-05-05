09daa1710b96c04eb9b2ce5f90271a48b3b284e8
Verchurro <berrygirault@gmail.com>
2023-05-05 18:30:06 +0200

    ui

--- Assets/Core/Berth/Scripts/MenuScreen.cs
+++ Assets/Core/Berth/Scripts/MenuScreen.cs
@@ -1,13 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuScreen : MonoBehaviour
 {
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Action"))
+        {
+            FadeToLevel(1);
+        }
+    }
+
+    public Animator animator;
     public void Play()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void FadeToLevel(int levelIndex)
+    {
+        animator.SetTrigger("FadeOut");
     }
 
     public void Quit()
@@ -15,4 +29,5 @@ public class MenuScreen : MonoBehaviour
         Debug.Log("left");
         Application.Quit();
     }
+
 }
