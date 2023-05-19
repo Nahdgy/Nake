4ff0e544aea45105cffa1db107398b4fc87706f6
Verchurro <berrygirault@gmail.com>
2023-05-19 14:43:35 +0200

    clock hands update

--- Assets/Core/Berth/Scripts/Clock.cs
+++ Assets/Core/Berth/Scripts/Clock.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Clock : MonoBehaviour
 {
     [SerializeField] Animator Animation;
-    
-
+    [SerializeField] bool trente = false, quinze = false;
+    public GameObject little, big;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,14 +23,44 @@ public class Clock : MonoBehaviour
        {
             Animation.speed = 0;
        }
+        Validation();
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag ("Time"))
+       // if (other.little.CompareTag ("Quinze"))
         {
-
+            Debug.Log("collide 15");
+            quinze = true;
         }
+        if (other.gameObject.CompareTag("Trente"))
+        {
+            Debug.Log("collide 30");
+            trente = true;
+        }
+
     }
 
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("Quinze"))
+        {
+            Debug.Log("out 15");
+            quinze = false;
+        }
+        if (other.gameObject.CompareTag("Trente"))
+        {
+            Debug.Log("out 30");
+            trente = false;
+        }
+
+    }
+    private void Validation()
+    {
+        if(quinze == true && trente == true) 
+        {
+            Debug.Log("Good");
+        }
+    }
 }
