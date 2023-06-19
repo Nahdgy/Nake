b9eb07538e447802b6bef9298103cee235bcb150
Verchurro <berrygirault@gmail.com>
2023-06-19 12:25:17 +0200

    fixing ui

--- Assets/Core/Nahdgy/Scripts/Enigmes/Ouija Board/LensNavigation.cs
+++ Assets/Core/Nahdgy/Scripts/Enigmes/Ouija Board/LensNavigation.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using static UnityEditor.Experimental.GraphView.GraphView;
+//using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class LensNavigation : MonoBehaviour,Icodable
 {
